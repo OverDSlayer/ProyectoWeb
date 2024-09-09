8004622f9388b4a060a5c2c0b6236ac083e5eb54
DiegoCaldas24 <juandiegoarmandoc@gmail.com>
2024-09-08 22:12:40 -0500

    Changes, carpeta de imagen y landing page

--- src/main.js
+++ src/main.js
@@ -1,1 +1,37 @@
-//TODO interactividad web
+/*
+const user = document.getElementById('user');
+const password = document.getElementById('password');
+const button = document.getElementById('button');
+
+button.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let userValue = user.value;
+    let passwordValue = password.value;
+
+    localStorage.setItem('user', userValue);
+    localStorage.setItem('password', passwordValue);
+
+    alert('Se agrego de manera correcta el usuario');
+})
+*/
+
+const user = document.getElementById('user');
+const password = document.getElementById('password');
+const button = document.getElementById('button');
+
+button.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    let userValue = user.value;
+    let passwordValue = password.value;
+
+    let userL = localStorage.getItem('user');
+    let passL = localStorage.getItem('password');
+
+
+    if(userValue === userL && passwordValue === passL){
+        alert('Tu Usuario es correcto')
+    }else{
+        alert('Tu Usuario no es correcto')
+    }
+})
