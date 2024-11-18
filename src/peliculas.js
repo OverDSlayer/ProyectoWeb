fc6d00ce3cb31f8d163c80785d2c2a53ebd7297a
StevenG4 <jozstevenn2004@gmail.com>
2024-11-17 19:08:16 -0500

    Revert "Merge branch 'main' of https://github.com/OverDSlayer/ProyectoWeb"
    This reverts commit aa80b9c668519107e618fc60cd4fea393c8c27a6, reversing
    changes made to 93d3784cd4d2893923495bea095dfc4957cda2b6.

--- src/peliculas.js
+++ src/peliculas.js
@@ -1,19 +1,29 @@
-document.querySelector('.paw').addEventListener('click', function () {
-  const contenedorGrid = document.querySelector('.pelicula');
-  const flecha = document.querySelector('.paw i'); // Seleccionamos el ícono dentro de la flecha
+const iPlay = document.querySelectorAll(".play");
 
-  // Alternar la clase 'oculto' en el contenedor de la cuadrícula
-  contenedorGrid.classList.toggle('oculto');
+iPlay.forEach(element => {
+    element.addEventListener('click', ()=>{
+        alert('Viendo peli...')
+    })
+})
 
-  // Cambiar el símbolo de la flecha (arriba/abajo) según si se muestra u oculta
-  if (contenedorGrid.classList.contains('oculto')) {
-    flecha.classList.remove('fa-chevron-up');
-    flecha.classList.add('fa-chevron-down');
-  } else {
-    flecha.classList.remove('fa-chevron-down');
-    flecha.classList.add('fa-chevron-up');
-  }
+
+document.querySelector('.paw').addEventListener('click', function() {
+    const contenedorGrid = document.querySelector('.pelicula');
+    const flecha = document.querySelector('.paw i'); // Seleccionamos el ícono dentro de la flecha
+
+    // Alternar la clase 'oculto' en el contenedor de la cuadrícula
+    contenedorGrid.classList.toggle('oculto');
+    
+    // Cambiar el símbolo de la flecha (arriba/abajo) según si se muestra u oculta
+    if (contenedorGrid.classList.contains('oculto')) {
+        flecha.classList.remove('fa-chevron-up');
+        flecha.classList.add('fa-chevron-down');
+    } else {
+        flecha.classList.remove('fa-chevron-down');
+        flecha.classList.add('fa-chevron-up');
+    }
 });
+
 
 const carousel = document.querySelector(".carousel");
 const imageContainer = document.querySelector(".image-container");
@@ -31,8 +41,9 @@ function slide(direction) {
   } else if (currentIndex >= images.length) {
     currentIndex = 0;
   }
-  imageContainer.style.transform = `translateX(-${currentIndex * imageWidth
-    }px)`;
+  imageContainer.style.transform = `translateX(-${
+    currentIndex * imageWidth
+  }px)`;
 }
 
 prevButton.addEventListener("click", () => slide(-1));
@@ -40,77 +51,3 @@ nextButton.addEventListener("click", () => slide(1));
 
 // Auto deslizar (ajusta el intervalo según tus preferencias)
 setInterval(() => slide(1), 3000);
-
-// Obtener las peliculas del back-end
-async function obtenerPeliculas(){
-  const data = await fetch('http://localhost:3000/peliculas');
-  const result = await data.json();
-  result.map((dato) => {
-    crearPelis(dato.id,dato.urlImg);
-  });
-}
-
-async function obtenerCuatroPeliculas(){
-  const data = await fetch('http://localhost:3000/for-peliculas');
-  const result = await data.json();
-  result.map((dato) => {
-    crearCuatroPelis(dato.id,dato.urlImg);
-  });
-}
-
-function crearCuatroPelis(id, img){
-  const $recomendado = document.querySelector('.recomendado');
-  const $peli_oculto = document.querySelector('.grid-container');
-
-  const $grid_item = document.createElement('div');
-  $grid_item.classList.add('grid-item');
-  const $img = document.createElement('img');
-  $img.setAttribute('src', img);
-  const $play = document.createElement('div');
-  $play.classList.add('play');
-  const $i = document.createElement('i');
-  $i.setAttribute('class', 'fa-solid fa-circle-play')
-  const $span = document.createElement('span');
-  $span.textContent = 'PLAY';
-  $play.append($i);
-  $play.append($span);
-  $grid_item.append($img);
-  $grid_item.append($play);
-  $peli_oculto.append($grid_item);
-
-  $play.addEventListener('click', ()=>{
-    localStorage.setItem('id', id);
-    // location.href = '' // pagina para ver peliculas
-  })
-}
-
-
-
-function crearPelis(id, img){
-  const $recomendado = document.querySelector('.recomendado');
-  const $peli_oculto = document.querySelector('.pelicula');
-
-  const $grid_item = document.createElement('div');
-  $grid_item.classList.add('grid-item');
-  const $img = document.createElement('img');
-  $img.setAttribute('src', img);
-  const $play = document.createElement('div');
-  $play.classList.add('play');
-  const $i = document.createElement('i');
-  $i.setAttribute('class', 'fa-solid fa-circle-play')
-  const $span = document.createElement('span');
-  $span.textContent = 'PLAY';
-  $play.append($i);
-  $play.append($span);
-  $grid_item.append($img);
-  $grid_item.append($play);
-  $peli_oculto.append($grid_item);
-
-  $play.addEventListener('click', ()=>{
-    localStorage.setItem('id', id);
-    // location.href = '' // pagina para ver peliculas
-  })
-}
-
-obtenerPeliculas();
-obtenerCuatroPeliculas();
